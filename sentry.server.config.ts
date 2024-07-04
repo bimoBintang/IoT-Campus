3bf17364e35664510a32fa0f8383532a8ca4db73
amcu <volkuy23@gmail.com>
2024-07-04 11:16:27 +0700

    Update Sentry

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://60c5699d30c3a3f99edb5975f6c2c27e@o4507535622471680.ingest.us.sentry.io/4507535625289728",
+  dsn: `${process.env.NEXT_PUBLIC_DSN_SENTRY}`,
 
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1,
